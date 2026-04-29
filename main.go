3cf3696ac3303a1d7080e6f1cf80583a2bafb611
Abhineshhh <abhinesh_223002@saitm.org>
2026-04-29 23:38:01 +0530

    added port config

--- main.go
+++ main.go
@@ -1,3 +1,22 @@
-func main() {
+package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("Lets make a RSS aggregator feed ! ")
+
+	godotenv.Load(".env")
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port : ", portString)
 }

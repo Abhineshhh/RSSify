4d0b57adc3b67897f2f30b9c46448c0fa6bfcbb6
Abhineshhh <abhinesh_223002@saitm.org>
2026-05-01 23:58:36 +0530

    used chi router for handlers and added json serialization handler

--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Lets make a RSS aggregator feed ! ")
 
 	godotenv.Load(".env")
 
@@ -18,5 +19,26 @@ func main() {
 		log.Fatal("PORT is not found in the environment")
 	}
 
-	fmt.Println("Port : ", portString)
+	router := chi.NewRouter() // creates new router object
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	srvr := &http.Server{ // connects router object to the http sever
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v ", portString)
+	err := srvr.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
